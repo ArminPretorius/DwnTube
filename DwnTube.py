06e06316648868af63d362d3a041331aa4568b52
Armin Pretorius <112197873+ArminPretorius@users.noreply.github.com>
2023-02-27 01:46:59 +0200

    Updated GUI

--- DwnTube.py
+++ DwnTube.py
@@ -14,7 +14,7 @@ class App:
     def __init__(self, master):
         self.master = master
         self.master.geometry("720x480")
-        ##self.master.resizable(False, False)
+        self.master.resizable(False, False)
         self.master.title("DwnTube")
 
         #Create a Label widget
@@ -29,23 +29,34 @@ class App:
         self.btnCont = ctk.CTkButton(self.master, text="->", width=100, command=self.btnCont_onClick)
         self.btnCont.place(x=510, y=240)
 
+        self.frmTitle = ctk.CTkFrame(self.master, width= 700, height = 60)
+        self.frmTitle.place(x=10, y=490)
+
         # Title Label
-        self.lblTitle = ctk.CTkLabel(self.master, width= 500, font=("Montserrat", 16))
-        self.lblTitle.place(x=110, y=480)
+        self.lblTitle = ctk.CTkLabel(self.frmTitle, width= 500, font=("Montserrat", 16))
+        self.lblTitle.place(x=100, y=5)
 
         # Channel Label
-        self.lblChannel = ctk.CTkLabel(self.master, width= 500, font=("Montserrat", 12))
-        self.lblChannel.place(x=110, y=480)
+        self.lblChannel = ctk.CTkLabel(self.frmTitle, width= 500, font=("Montserrat", 12))
+        self.lblChannel.place(x=100, y=30)
 
-        self.lblThumbnail = ctk.CTkLabel(self.master, text="")
-        self.lblThumbnail.place(x=20, y=560)
+        self.frmInfo = ctk.CTkFrame(self.master, width= 345, height= 350)
+        self.frmInfo.place(x= 10, y= 520)
 
-        self.lblDescription = ctk.CTkLabel(self.master, text="test")
-        self.lblDescription.place(x=20, y = 800)
-    
+        self.frmDownload = ctk.CTkFrame(self.master, width= 345, height= 350)
+        self.frmDownload.place(x= 365, y= 520)
+
+        self.lblThumbnail = ctk.CTkLabel(self.frmInfo, text="")
+        self.lblThumbnail.place(x=10, y=10)
+
+        self.lblDescription = ctk.CTkLabel(self.frmInfo, text="", wraplength=325, justify="left")
+        self.lblDescription.place(x=10, y = 190)
+
+        self.btnDownload = ctk.CTkButton(self.frmDownload, text="Download", command=print("You clicked download"))
+        self.btnDownload.place(x=100, y=310)
 
     def btnCont_onClick(self):
-        #self.get_video_info()
+        self.get_video_info()
         self.animateUp()
 
 
@@ -63,14 +74,13 @@ class App:
         channel_name = video_info.get('channel', None)
         # Print the video title
         self.lblTitle.configure(text=video_title)
-        print(video_description)
-        print(video_thumbnail)
+        self.lblDescription.configure(text=video_description)
         self.lblChannel.configure(text="By: "+channel_name)
         # Download thumbnail image using urllib
         with urllib.request.urlopen(video_thumbnail) as url:
             thumbnail_image = Image.open(url)
         # Convert thumbnail image to Tkinter-compatible format
-        thumbnail_tk = ImageTk.PhotoImage((thumbnail_image).resize((384,216),Image.Resampling.NEAREST))
+        thumbnail_tk = ImageTk.PhotoImage((thumbnail_image).resize((320,180),Image.Resampling.NEAREST))
         self.lblThumbnail.configure(image=thumbnail_tk)
 
     def animateUp(self):
@@ -79,17 +89,15 @@ class App:
             self.lblLink.place(y=200-i*10)
             self.entLink.place(y=240-i*10)
             self.btnCont.place(y=240-i*10)
-            self.lblTitle.place(y=480-i*10)
-            self.lblChannel.place(y=510-i*10)
-            self.lblThumbnail.place(y=560-i*10)
-            self.lblDescription.place(y=760-i*10)
+            self.frmTitle.place(y=480-i*10)
+            self.frmInfo.place(y=550-i*10)
+            self.frmDownload.place(y=550-i*10)
             self.master.update()
             self.master.after(20)
         for i in range(20):
-            self.lblTitle.place(y=240-i*10)
-            self.lblChannel.place(y=270-i*10)
-            self.lblThumbnail.place(y=320-i*10)
-            self.lblDescription.place(y=520-i*10)
+            self.frmTitle.place(y=240-i*10)
+            self.frmInfo.place(y=310-i*10)
+            self.frmDownload.place(y=310-i*10)
             self.master.update()
             self.master.after(20)
 
